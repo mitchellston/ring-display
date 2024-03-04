5c0444c5fb6ab6c5e553d8cb8155f921e625b851
mitchellston <m.steenwijk@live.nl>
2024-03-04 19:32:01 +0100

    auto update

---
+++ startup.sh
@@ -0,0 +1,19 @@
+# Start docker containers
+(cd "$(dirname "$0")/run" && docker-compose build && docker-compose up -d)
+
+# check if cron is installed and install it if not (for debian based systems and mac)
+if ! [ -x "$(command -v cron)" ]; then
+  if [ -x "$(command -v apt-get)" ]; then
+    apt-get update
+    apt-get install -y cron
+  elif [ -x "$(command -v brew)" ]; then
+    brew install cron
+  else
+    echo 'Error: cron is not installed and we cannot install it' >&2
+    (cd "$(dirname "$0")/run" && docker-compose down)
+    exit 1
+  fi
+fi
+
+# Create a cron job that runs every 1 hour to update the modules and restart the container if necessary
+(crontab -l 2>/dev/null; echo "0 * * * * $(dirname "$0")/update.sh") | crontab -
---
+++ update.sh
@@ -0,0 +1,42 @@
+# check if there are any changes on origin
+if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
+    echo "There are changes on origin"
+    git pull
+    echo "Pulled the latest changes"
+    (cd "$(dirname "$0")/run" && docker-compose build && docker-compose up -d)
+fi
+
+# Check if there are any changes in the modules
+CHANGES=false
+while read -r module; do
+    # Skip empty lines and comments
+    if [ -z "$module" ] || [[ "$module" =~ ^# ]] ; then
+        continue
+    fi
+    # Split by space and get the first element
+    url=$(echo $module | awk '{print $1}')
+    name=$(echo $module | awk '{print $2}')
+    version=$(echo $module | awk '{print $3}')
+    # if module already exists.
+    if [ -d "$(dirname "$0")/mounts/modules/$name" ]; then
+        # check if the module is the right git repo
+        if [ "$(cd $(dirname "$0")/mounts/modules/$name && git remote get-url origin)" != "$url" ]; then
+            rm -rf "$(dirname "$0")/mounts/modules/$name"
+            CHANGES=true
+        else
+            # Check if there are any changes
+            if [ $(cd $(dirname "$0")/mounts/modules/$name && git rev-parse HEAD) != $(cd $(dirname "$0")/mounts/modules/$name && git rev-parse @{u}) ]; then
+                CHANGES=true
+            fi
+        fi
+    else 
+        CHANGES=true
+    fi
+done < $(dirname "$0")/mounts/modules/modules
+
+# If there are changes, update the modules and restart the container
+if [ "$CHANGES" = true ]; then
+    echo "There are changes in the modules"
+    (cd "$(dirname "$0")/mounts/modules" && ./install.sh)
+    (cd "$(dirname "$0")/run" && docker-compose build && docker-compose up -d)
+fi
