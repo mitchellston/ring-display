7a7ef617cdc7f9dc2e099fda77c5839c44b70468
mitchellston <m.steenwijk@live.nl>
2024-04-19 21:12:11 +0200

    no interact

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,12 +1,12 @@
 # Setup ssh server
 sudo apt-get update
-sudo apt-get install openssh-server
+sudo apt-get install openssh-server -y
 sudo systemctl enable ssh
 sudo systemctl start ssh
 
 # Docker install
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -17,4 +17,4 @@ echo \
 sudo apt-get update
 
 # Docker extension install
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
