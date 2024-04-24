84d3ff2c050d66e54162a6efd9fbf3317482c5a3
mitchellston <m.steenwijk@live.nl>
2024-04-24 22:00:09 +0200

    decrease sleep

--- run/runtime-start.sh
+++ run/runtime-start.sh
@@ -4,7 +4,7 @@ xset -dpms
 xset q
 # Install the modules
 /opt/magic_mirror/modules/install.sh
-# Sleep for 30 seconds to allow the modules to install and not interfere with the network of the MagicMirror
-sleep 30
+# Sleep for 5 seconds to allow the modules to install and not interfere with the network of the MagicMirror
+sleep 5
 # Start the MagicMirror
 npm run start
